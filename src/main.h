963695277cba25cbd0d0ee931d2b8bd288c06856
we7u <we7u>
2002-04-05 22:50:05 +0000

    Modifying zooming in with the mouse such that it now figures out whether most of the mouse movement was in the horizontal or in the vertical direction, then computes the new zoom level based on that.  Previous to this change zooming was calculated based only on vertical movement.

--- src/main.h
+++ src/main.h
@@ -1,5 +1,5 @@
 /*
- * $Id: main.h,v 1.5 2002/03/27 07:57:31 we7u Exp $
+ * $Id: main.h,v 1.6 2002/04/05 22:50:05 we7u Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -204,6 +204,7 @@ extern void location_gui_init(void);
 extern void view_message_gui_init(void);
 extern void wx_gui_init(void);
 extern long get_x_scale(long x, long y, long ysc);
+extern long get_y_scale(long x, long y, long xsc);
 
 extern Widget Display_data_dialog;
 extern Widget Display_data_text;
