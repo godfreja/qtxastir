09e48b084808787d1950fa757cf2bb017c2a47c1
we7u <we7u>
2002-04-09 20:34:09 +0000

    Moving "default:" cases to end of switch statements.

--- src/interface_gui.c
+++ src/interface_gui.c
@@ -1,5 +1,5 @@
 /*
- * $Id: interface_gui.c,v 1.8 2002/04/09 09:25:14 we7u Exp $
+ * $Id: interface_gui.c,v 1.9 2002/04/09 20:34:09 we7u Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -2043,11 +2043,6 @@ begin_critical_section(&devices_lock, "interface_gui.c:Config_WX" );
                     break;
             }
             switch (WX_rain_gauge_type) {
-                case(0):
-                default:
-                    XmToggleButtonSetState(WX_tenths,TRUE,FALSE);
-//                    WX_rain_gauge_type=0;
-                    break;
                 case(1):
                     XmToggleButtonSetState(WX_hundredths,TRUE,FALSE);
 //                    WX_rain_gauge_type=1;
@@ -2055,6 +2050,11 @@ begin_critical_section(&devices_lock, "interface_gui.c:Config_WX" );
                 case(2):
                     XmToggleButtonSetState(WX_millimeters,TRUE,FALSE);
 //                    WX_rain_gauge_type=2;
+                    break;
+                case(0):
+                default:
+                    XmToggleButtonSetState(WX_tenths,TRUE,FALSE);
+//                    WX_rain_gauge_type=0;
                     break;
             }
 
