0b32b4490a83a0bc3efd92220e2376601c826399
we7u <we7u>
2002-04-25 07:00:05 +0000

    Fixes for Shapefile segfaults:  Can't retrieve a field that's higher than fieldcount, else segfault.

--- src/maps.h
+++ src/maps.h
@@ -1,5 +1,5 @@
 /*
- * $Id: maps.h,v 1.2 2002/04/24 23:00:19 we7u Exp $
+ * $Id: maps.h,v 1.3 2002/04/25 07:00:05 we7u Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -28,7 +28,7 @@
 #include <X11/Intrinsic.h>
 
 #define MAX_OUTBOUND 900
-#define MAX_MAP_POINTS 20000
+#define MAX_MAP_POINTS 100000
 
 #define DRAW_TO_PIXMAP          0
 #define DRAW_TO_PIXMAP_FINAL    1
