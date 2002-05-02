d1f873659a2c177bacee87e6f5e131f9d82a7020
we7u <we7u>
2002-05-02 23:53:06 +0000

    This ones for Ken:  Download and select plc00 files from GeographyNetwork to get the "designated places" boundaries filled in with PaleGoldenrod color.  The code will also label these areas.  Create the following subdirectories to get the layering right:
        county (cty00)
        designated_places (plc00)
        h2o (lkH and wat)
        misc (lkC)
        rail (lkB)
        roads (lkA)

--- src/maps.c
+++ src/maps.c
@@ -1,5 +1,5 @@
 /* -*- c-basic-indent: 4; indent-tabs-mode: nil -*-
- * $Id: maps.c,v 1.75 2002/05/02 21:25:44 we7u Exp $
+ * $Id: maps.c,v 1.76 2002/05/02 23:53:06 we7u Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -1348,12 +1348,13 @@ void draw_shapefile_map (Widget w,
 
         // Check the filename for mapshots.com filetypes to see what
         // type of file we may be dealing with.
-        if (strncasecmp(filename,"Tgr",3) == 0) {   // Found a Mapshots file
+        if (strncasecmp(filename,"tgr",3) == 0) {   // Found Mapshots or GeographyNetwork file
 
-            if (strstr(filename,"ccdcu")) {         // Cities:  Arlington
+            if (strstr(filename,"plc")) {         // Designated Places:  Arlington
+                city_flag++;
+                mapshots_labels_flag++;
                 if (debug_level & 16) {
-                    city_flag++;
-                    printf("*** Found (mapshots political subdivisions: Cities) ***\n");
+                    printf("*** Found (Designated Places) ***\n");
                     break;
                 }
                 else
@@ -2466,8 +2467,12 @@ void draw_shapefile_map (Widget w,
                                 (void)XDrawLines(XtDisplay(w), pixmap_alerts, gc_tint, points, i, CoordModeOrigin);
                             }
                             else if (map_color_fill) {  // Land masses?
-                                (void)XSetForeground(XtDisplay(w), gc, colors[0x0f]); // white
-(void)XSetForeground(XtDisplay(w), gc, colors[0xff]); // grey
+                                if (city_flag) {
+                                    (void)XSetForeground(XtDisplay(w), gc, GetPixelByName(w,"PaleGoldenrod")); // PaleGoldenrod
+                                }
+                                else {
+                                    (void)XSetForeground(XtDisplay(w), gc, colors[0xff]); // grey
+                                }
 
                                 (void)XFillPolygon(XtDisplay (w), pixmap, gc, points, i, Complex, CoordModeOrigin);
                                 (void)XSetForeground(XtDisplay(w), gc, colors[0x08]); // black for border
@@ -2489,6 +2494,14 @@ void draw_shapefile_map (Widget w,
                             temp = DBFReadStringAttribute( hDBF, structure, 3 );
                         else if (fieldcount >= 1)
                             temp = DBFReadStringAttribute( hDBF, structure, 0 );    // NAME (lakes)
+                        else
+                            temp = NULL;
+                    }
+                    else if (city_flag) {
+                        if (map_color_levels && scale_y > 512)
+                            skip_label++;
+                        if (mapshots_labels_flag && (fieldcount >= 4) )
+                            temp = DBFReadStringAttribute( hDBF, structure, 3 );    // NAME (designated places)
                         else
                             temp = NULL;
                     }
