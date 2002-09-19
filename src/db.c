ccf78e12c8d685004c707ec64d5c07b12300c175
we7u <we7u>
2002-09-19 20:49:29 +0000

    More careful string copying of callsigns in a couple of places.  There was a possibility of overrunning the destination string before.

--- src/db.c
+++ src/db.c
@@ -1,5 +1,5 @@
 /* -*- c-basic-indent: 4; indent-tabs-mode: nil -*-
- * $Id: db.c,v 1.139 2002/09/19 20:27:57 we7u Exp $
+ * $Id: db.c,v 1.140 2002/09/19 20:49:29 we7u Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -10348,9 +10348,9 @@ int extract_third_party(char *call, char *path, char **info, char *origin) {
     if (ok) {                                         // check callsign
         (void)remove_trailing_asterisk(p_call);       // is an asterisk valid here ???
         if (valid_inet_name(p_call,(*info),origin)) { // accept some of the names used in internet
-            strcpy(call,p_call);                      // treat is as object with special origin
+            strncpy(call,p_call,MAX_CALLSIGN+1);      // treat is as object with special origin
         } else if (valid_call(p_call)) {              // accept real AX.25 calls
-            strcpy(call,p_call);
+            strncpy(call,p_call,MAX_CALLSIGN+1);
         } else {
             ok = 0;
             if (debug_level & 1) {
