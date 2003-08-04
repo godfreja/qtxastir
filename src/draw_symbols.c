3f72527608dc909cbda1fba419f7e6cf67edbd67
we7u <we7u>
2003-08-04 22:36:58 +0000

    Changing draw colors only when necessary.  Found this one while profiling using gprof.  May be more to change in this procedure to speed things up a bit..

--- src/draw_symbols.c
+++ src/draw_symbols.c
@@ -1,5 +1,5 @@
 /*
- * $Id: draw_symbols.c,v 1.44 2003/07/10 19:00:54 we7u Exp $
+ * $Id: draw_symbols.c,v 1.45 2003/08/04 22:36:58 we7u Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -1573,7 +1573,7 @@ void load_pixmap_symbol_file(char *filename) {
 
 /* add a symbol to the end of the symbol table */
 void insert_symbol(char table, char symbol, char *pixel, int deg, char orient) {
-    int x,y,idx,old_next,color;
+    int x,y,idx,old_next,color,last_color;
 
     if (symbols_loaded < MAX_SYMBOLS) {
         symbol_data[symbols_loaded].pix=XCreatePixmap(XtDisplay(appshell),
@@ -1595,7 +1595,11 @@ void insert_symbol(char table, char symbol, char *pixel, int deg, char orient) {
                 1);
 
         old_next=0;
+        last_color = -1;    // Something bogus
+
         for (y=0;y<20;y++) {
+
+// Identified as a high CPU-usage area by gprof!!
             for (x=0;x<20;x++) {
                 switch (deg) {
                     case(0):
@@ -1615,24 +1619,28 @@ void insert_symbol(char table, char symbol, char *pixel, int deg, char orient) {
                 if (color<0)
                     color = 0xff;
 
+                // Change to new color only when necessary.
                 // DK7IN: is (da) correct or should this be (appshell) ?
-                (void)XSetForeground(XtDisplay(da),gc,colors[color]);
-                (void)XDrawPoint(XtDisplay(da),symbol_data[symbols_loaded].pix,gc,x,y);
+                if (color != last_color) {
+(void)XSetForeground(XtDisplay(da),gc,colors[color]);
+                    last_color = color;
+                }
+(void)XDrawPoint(XtDisplay(da),symbol_data[symbols_loaded].pix,gc,x,y);
                 // DK7IN
 
                 if (color != 0xff)     // create symbol mask
-                    (void)XSetForeground(XtDisplay(appshell),gc2,1);  // active bit
+(void)XSetForeground(XtDisplay(appshell),gc2,1);  // active bit
                 else
-                    (void)XSetForeground(XtDisplay(appshell),gc2,0);  // transparent
+(void)XSetForeground(XtDisplay(appshell),gc2,0);  // transparent
 
-                (void)XDrawPoint(XtDisplay(appshell),symbol_data[symbols_loaded].pix_mask,gc2,x,y);
+(void)XDrawPoint(XtDisplay(appshell),symbol_data[symbols_loaded].pix_mask,gc2,x,y);
 
                 old_next++;        // create ghost symbol mask
                 if (old_next>1) {  // by setting every 2nd bit to transparent
                     old_next=0;
-                    (void)XSetForeground(XtDisplay(appshell),gc2,0);
+(void)XSetForeground(XtDisplay(appshell),gc2,0);
                 }
-                (void)XDrawPoint(XtDisplay(appshell),symbol_data[symbols_loaded].pix_mask_old,gc2,x,y);
+(void)XDrawPoint(XtDisplay(appshell),symbol_data[symbols_loaded].pix_mask_old,gc2,x,y);
             }
             old_next++;    // shift one bit every scan line for ghost image
             if (old_next>1)
