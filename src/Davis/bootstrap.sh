04f7ae9e5fe2108a12fa6c99d65288602a6b87b7
we7u <we7u>
2004-02-04 20:18:24 +0000

    Adding new files to this project for the first time.

---
+++ src/Davis/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# $Id: bootstrap.sh,v 1.1 2004/02/04 20:18:24 we7u Exp $
+#
+# Copyright (C) 2000-2004  The Xastir Group
+#
+#
+# This simple routine will run autostuff in the appropriate
+# order to generate the needed configure/makefiles
+#
+
+echo "Removing autom4te.cache directory"
+rm -rf autom4te.cache
+
+echo "Running aclocal"
+aclocal
+
+echo "Running autoheader"
+autoheader
+
+echo "Running autoconf"
+autoconf
+
+# Cygwin needs these params to be separate
+echo "Running automake"
+automake -a -c
+
