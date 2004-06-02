e110bfea5ae21719cd112ceeb53c0e70ac0d3aa1
we7u <we7u>
2004-06-02 16:38:53 +0000

    Moved these from xastir/src/Davis/src to xastir/Davis/src

---
+++ Davis/src/defs.h
@@ -0,0 +1,14 @@
+
+// $Id: defs.h,v 1.1 2004/06/02 16:38:53 we7u Exp $
+//
+// Copyright (C) 2004 Bruce Bennett <bruts@adelphia.net>
+//
+//
+// Dummy info here for now...
+//
+
+
+#define VERSION "0.1.1"
+#define PORT	1313
+#define CONNECTIONS	20
+#define NOFILE		20
