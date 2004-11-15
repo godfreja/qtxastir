75e0da805196d51be7cb14f9f8938bbe2096a1ee
we7u <we7u>
2004-11-15 16:52:24 +0000

    Moving more of the includes inside the #ifdef.  Including config.h so we get a direct answer as to whether USE_MAP_CACHE is defined before we include anything else.

--- src/map_cache.c
+++ src/map_cache.c
@@ -1,5 +1,5 @@
 /*
- * $Id: map_cache.c,v 1.5 2004/11/15 16:22:15 we7u Exp $
+ * $Id: map_cache.c,v 1.6 2004/11/15 16:52:24 we7u Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -25,6 +25,13 @@
 
 
 
+// Need this one before the #ifdef in order to get the definition of
+// USE_MAP_CACHE, if defined.
+#include "config.h"
+
+#ifdef  USE_MAP_CACHE
+#warning USE_MAP_CACHE Defined (and there was much rejoicing) 
+
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -37,12 +44,6 @@
 #include "xastir.h"
 #include "xa_config.h" 
 #include "maps.h" 
-
-
-
-#ifdef  USE_MAP_CACHE
-#warning USE_MAP_CACHE Defined (and there was much rejoicing) 
-
 #include "map_cache.h" 
 #include <db.h>
 
