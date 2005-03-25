75da94c81f16d528c0a70115e1bfe2ede2618d3b
we7u <we7u>
2005-03-25 20:53:04 +0000

    Changes to the comments/description.

--- scripts/permutations.pl
+++ scripts/permutations.pl
@@ -6,8 +6,16 @@
 # Released to the public domain.
 #
 #
-# $Id: permutations.pl,v 1.8 2003/01/06 22:42:16 we7u Exp $
+# $Id: permutations.pl,v 1.9 2005/03/25 20:53:04 we7u Exp $
 #
+#
+# Finds the different lat/long representations corresponding to the
+# input numbers.  A space is required between the degrees portion
+# and the rest of the input.  Writes out a log file containing APRS
+# objects suitable for importing into Xastir, to graphically plot
+# the locations of the objects.  Now that Xastir has a server port
+# we could directly inject them into the program via that route as
+# well, but we currently don't do that.
 #
 # Converts between different lat/lon formats.  Will also give UMS
 # position if the lat/lon resides somewhere inside the Seattle area
