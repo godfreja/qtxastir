a370847c50e9f3bfb64574c26f38546c97aa7b1f
we7u <we7u>
2005-10-03 19:51:05 +0000

    A slightly better system for controlling left/right edges of map when at left/right edge of world.  check_range().

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /* -*- c-basic-offset: 4; indent-tabs-mode: nil -*-
- * $Id: main.c,v 1.636 2005/10/03 18:34:25 we7u Exp $
+ * $Id: main.c,v 1.637 2005/10/03 19:51:05 we7u Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -11393,6 +11393,7 @@ void check_range(void) {
 // have to test the y scaling again?
 
 
+/*
     // Check against left border
     if ((new_mid_x < (width*new_scale_x)/2)) {
         // This will cause the map image to snap to the left of the
@@ -11406,6 +11407,22 @@ void check_range(void) {
             // the display.
             new_mid_x = 129600000l-((width*new_scale_x)/2); // right border max 180°E
     }
+*/
+
+
+// long x_long_offset;             // Longitude at top NW corner of map screen
+// long y_lat_offset;              // Latitude  at top NW corner of map screen
+
+if (x_long_offset < 0l) {
+//    fprintf(stderr,"left\n");
+    x_long_offset = 0l;         // New left viewpoint edge
+    new_mid_x = 0l + ((width*new_scale_x) / 2); // New midpoint
+}
+if ( (x_long_offset + (width*new_scale_x) ) > 129600000l) {
+//    fprintf(stderr,"right\n");
+    x_long_offset = 129600000l - (width*new_scale_x);   // New left viewpoint edge
+    new_mid_x = 129600000l - ((width*new_scale_x) / 2); // New midpoint
+}
 
 // Find the four corners of the map in the new scale system.  Make
 // sure they are on the display, but not well inside the borders of
