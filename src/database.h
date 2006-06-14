00ca4ec9ee90c5e0a105204c8cf355c6bb49588b
we7u <we7u>
2006-06-14 20:55:39 +0000

    Doesn't make much sense to add the new parameter to the station database at this time when code doesn't use it.  It would only take up memory. Commenting it out for now until we get to using it.

--- src/database.h
+++ src/database.h
@@ -1,5 +1,5 @@
 /*
- * $Id: database.h,v 1.28 2006/06/14 20:50:37 we7u Exp $
+ * $Id: database.h,v 1.29 2006/06/14 20:55:39 we7u Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -370,7 +370,7 @@ typedef struct _DataRow {
     char pos_amb;               // Position ambiguity, 0 = none,
                                 // 1 = 0.1 minute...
 
-    unsigned int error_elipse_radius;    // Degrades precision for
+//    unsigned int error_elipse_radius;    // Degrades precision for
                                 // this station, from 0 to 65535 cm
                                 // or 655.35 meters.  Assigned when
                                 // we decode each type of packet.
