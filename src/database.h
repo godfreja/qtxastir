d502ffb9b855c8af8fd0ed19105e0ad1d9a69bac
tvrusso <tvrusso>
2007-03-31 19:49:40 +0000

    Add some sanity checking to the EXPIRE_DEBUG logic.
    I am still seeing that every few weeks my xastir run starts to have stations
    that should have expired 10 days ago, but still has 'em.  This really screws
    up the ALOHA calculations.
    
    This sanity checking just goes through the entire time-sorted list and checks
    to see if any stations are older than the expire time, and does this immediately
    after the expiration is processed.  The sanity checking only happens when
    EXPIRE_DEBUG is defined.
    
    The expire code assumes that the list is sorted by time, and stops when it finds
    a station newer than the expire time.  If the list is somehow getting
    corrupted and no longer properly sorted, that would explain the behavior I'm
    seeing.  I still haven't found the problem, but this should at least tell me
    if the list management has a bug or not.

--- src/database.h
+++ src/database.h
@@ -1,5 +1,5 @@
 /*
- * $Id: database.h,v 1.39 2007/03/08 05:17:10 tvrusso Exp $
+ * $Id: database.h,v 1.40 2007/03/31 19:49:40 tvrusso Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -640,4 +640,6 @@ void Show_Aloha_Stats(Widget w, XtPointer clientData,
                       XtPointer callData); // popup window
 
 int comp_by_dist(const void *,const void *);// used only for qsort
+DataRow * sanity_check_time_list(time_t); // used only for debugging
+
 #endif /* XASTIR_DATABASE_H */
