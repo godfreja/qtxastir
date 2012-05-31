334ce0f8db4183a59838875b4c5d72061397c151
tvrusso <tvrusso>
2012-05-31 18:49:52 +0000

    Fix incorrect test of sscanf return value.  When I changed the scanf to ignore the PXXX field, I forgot to decrease the value.

--- src/wx.c
+++ src/wx.c
@@ -1,5 +1,5 @@
 /*
- * $Id: wx.c,v 1.76 2012/05/31 18:41:35 tvrusso Exp $
+ * $Id: wx.c,v 1.77 2012/05/31 18:49:52 tvrusso Exp $
  *
  * XASTIR, Amateur Station Tracking and Information Reporting
  * Copyright (C) 1999,2000  Frank Giannandrea
@@ -2823,7 +2823,7 @@ void wx_fill_data(int from, int type, unsigned char *data, DataRow *fill) {
                        weather->wx_rain,
                        weather->wx_rain_total,
                        weather->wx_hum,
-                       weather->wx_baro) == 9){
+                       weather->wx_baro) == 8){
               // then we got all the data out of the packet... now process
               // First null-terminate all the strings:
               weather->wx_course[3]='\0';
@@ -2937,7 +2937,7 @@ void wx_fill_data(int from, int type, unsigned char *data, DataRow *fill) {
               
               
               if (debug_level & 1)
-                fprintf(stdout,"Davis APRS DataLogger Decode $Revision: 1.76 $: wd-%s,ws-%s,wg-%s,t-%s,rh-%s,rt-%s,h-%s,ap-%s,station-%s\n",
+                fprintf(stdout,"Davis APRS DataLogger Decode $Revision: 1.77 $: wd-%s,ws-%s,wg-%s,t-%s,rh-%s,rt-%s,h-%s,ap-%s,station-%s\n",
                         
                         weather->wx_course,weather->wx_speed,weather->wx_gust,
                         weather->wx_temp, weather->wx_rain,
